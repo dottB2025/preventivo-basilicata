764891f54c9f26330e397e6907facbba6f9343af
dottB2025 <giovanni.buonsanti@gmail.com>
2025-04-01 13:02:16 +0200

    Preventivi_Basilicata.py

--- Preventivi_Basilicata.py
+++ Preventivi_Basilicata.py
@@ -29,45 +29,12 @@ def genera_preventivo_da_dettato(testo: str, df: pd.DataFrame) -> str:
 st.set_page_config(page_title="Preventivo Sanitario Basilicata", layout="centered")
 st.title("Preventivo Sanitario - Basilicata")
 
-st.markdown("Inserisci o detta i codici regionali separati da virgola.\n" 
+st.markdown("Inserisci i codici regionali separati da virgola.\n" 
             "Puoi usare anche punti o spazi tra i numeri.\n" 
             "Esempio: 3.0.0.12.31, 3.0.0.13.82")
 
-# Area vocale (HTML+JS personalizzato)
-st.markdown("""
-<script>
-let recognition;
-function startDictation() {
-  if (!('webkitSpeechRecognition' in window)) {
-    alert("Il tuo browser non supporta la dettatura vocale.");
-  } else {
-    recognition = new webkitSpeechRecognition();
-    recognition.continuous = false;
-    recognition.interimResults = false;
-    recognition.lang = "it-IT";
-    recognition.start();
-
-    recognition.onresult = function(e) {
-      document.getElementById('voice_output').value = e.results[0][0].transcript;
-      recognition.stop();
-    };
-
-    recognition.onerror = function(e) {
-      recognition.stop();
-    }
-  }
-}
-</script>
-<button onclick="startDictation()">🎙️ Detta ora</button>
-<br><br>
-<textarea id="voice_output" rows="2" style="width: 100%;"></textarea>
-<form action="" method="get">
-  <input type="submit" value="Usa questo testo" />
-</form>
-""", unsafe_allow_html=True)
-
 # Input manuale in Streamlit
-input_codici = st.text_input("Oppure scrivi qui i codici regionali:")
+input_codici = st.text_input("Scrivi qui i codici regionali:")
 
 # Bottone di elaborazione
 if st.button("Genera Preventivo") or input_codici:
